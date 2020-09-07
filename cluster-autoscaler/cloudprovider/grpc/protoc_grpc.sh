b704e5603eabcb8d790be7cdd7b19829a2a6704f
fboltz <frederic.boltz@gmail.com>
2020-09-07 10:07:32 +0200

    Update

--- cluster-autoscaler/cloudprovider/grpc/protoc_grpc.sh
+++ cluster-autoscaler/cloudprovider/grpc/protoc_grpc.sh
@@ -4,9 +4,8 @@ PB_RELEASE="3.13.0"
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 PROTOC_DIR=$(mktemp -d /tmp/protoc-${PB_RELEASE}-XXXX)
 
-go get -v github.com/golang/protobuf/protoc-gen-go@v1.4.2
-
 pushd $PROTOC_DIR
+go get -v github.com/golang/protobuf/protoc-gen-go@v1.4.2
 curl -LO ${PB_REL}/download/v${PB_RELEASE}/protoc-${PB_RELEASE}-linux-x86_64.zip
 unzip protoc-${PB_RELEASE}-linux-x86_64.zip
 popd
