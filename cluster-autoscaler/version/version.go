e5a07e073421093e43845610dc88279ae5d3d9b1
fboltz <frederic.boltz@gmail.com>
2020-11-30 18:14:12 +0100

    FIX: Version 1.17.14

--- cluster-autoscaler/version/version.go
+++ cluster-autoscaler/version/version.go
@@ -17,4 +17,4 @@ limitations under the License.
 package version
 
 // ClusterAutoscalerVersion contains version of CA.
-const ClusterAutoscalerVersion = "1.17.12"
+const ClusterAutoscalerVersion = "1.17.14"
