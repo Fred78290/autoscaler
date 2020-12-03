b2973f5a1af520e531cc16074d12c6cbf6d9b6e5
fboltz <frederic.boltz@gmail.com>
2020-12-03 23:50:30 +0100

    FEAT: Build arch arm64

--- cluster-autoscaler/build.sh
+++ cluster-autoscaler/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo rm -rf out
-make -e REGISTRY=fred78290 -e TAG=v1.16.15 container
+make -e REGISTRY=fred78290 -e TAG=v1.16.15 container -e GOARCH=amd64
+make -e REGISTRY=fred78290 -e TAG=v1.16.15 container -e GOARCH=arm64
