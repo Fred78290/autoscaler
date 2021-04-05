7e591fcb86f5fcf70f83bb05100b4cd7eacf8991
fboltz <frederic.boltz@gmail.com>
2021-04-05 11:27:59 +0200

    FEAT: Update version

--- cluster-autoscaler/build.sh
+++ cluster-autoscaler/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo rm -rf out
-make -e REGISTRY=fred78290 -e TAG=v1.20.0 container -e GOARCH=amd64
-make -e REGISTRY=fred78290 -e TAG=v1.20.0 container -e GOARCH=arm64
+make -e REGISTRY=fred78290 -e TAG=v1.20.5 container -e GOARCH=amd64
+make -e REGISTRY=fred78290 -e TAG=v1.20.5 container -e GOARCH=arm64
