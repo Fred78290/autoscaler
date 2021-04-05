c6c8dfcf7c7c49ba07a02f9e13318563e9d67f43
fboltz <frederic.boltz@gmail.com>
2021-04-05 12:16:57 +0200

    FEAT: Build version 1.20.5

--- cluster-autoscaler/build.sh
+++ cluster-autoscaler/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 sudo rm -rf out
-make -e REGISTRY=fred78290 -e TAG=v1.20.5 container -e GOARCH=amd64
-make -e REGISTRY=fred78290 -e TAG=v1.20.5 container -e GOARCH=arm64
+VERSION=v1.20.5
+make -e REGISTRY=fred78290 -e TAG=$VERSION container -e GOARCH=amd64
+make -e REGISTRY=fred78290 -e TAG=$VERSION container -e GOARCH=arm64
+
+docker push fred78290/cluster-autoscaler:$VERSION-amd64
+docker push fred78290/cluster-autoscaler:$VERSION-arm64
+
+docker manifest create fred78290/cluster-autoscaler:$VERSION --amend fred78290/cluster-autoscaler:$VERSION-amd64 --amend fred78290/cluster-autoscaler:$VERSION-arm64
+docker manifest push fred78290/cluster-autoscaler:$VERSION
