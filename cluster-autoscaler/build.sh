5fc1f08b868891033be8a7263f809749f16f901f
fboltz <frederic.boltz@gmail.com>
2021-04-11 16:39:54 +0200

    FEAT: Kubernetes v1.21.0

--- cluster-autoscaler/build.sh
+++ cluster-autoscaler/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo rm -rf out
-make -e REGISTRY=fred78290 -e TAG=v1.21.0-beta.0 container
+
+make -e REGISTRY=fred78290 -e TAG=v1.21.0 container -e GOARCH=amd64
+make -e REGISTRY=fred78290 -e TAG=v1.21.0 container -e GOARCH=arm64
