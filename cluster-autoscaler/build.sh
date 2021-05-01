bcba9abe98405680358563f41d66cfb468806cd9
fboltz <frederic.boltz@gmail.com>
2021-05-01 11:08:03 +0200

    Build both arch

--- cluster-autoscaler/build.sh
+++ cluster-autoscaler/build.sh
@@ -4,7 +4,8 @@ sudo rm -rf out
 VERSION=v1.21.0
 REGISTRY=fred78290
 
-make -e REGISTRY=$REGISTRY -e TAG=$VERSION -e DOCKER_RM=1 container
+make -e REGISTRY=$REGISTRY -e TAG=$VERSION -e DOCKER_RM=1 container -e GOARCH=amd64
+make -e REGISTRY=$REGISTRY -e TAG=$VERSION -e DOCKER_RM=1 container -e GOARCH=arm64
 
 mkdir -p out/linux/amd64 out/linux/arm64
 cp cluster-autoscaler-amd64 out/linux/amd64/cluster-autoscaler
